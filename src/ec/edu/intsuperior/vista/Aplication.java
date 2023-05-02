90e8161baea33d42eb8c08b0a8a083dd49a88b58
Usuario <Usuario@JeanD7.lan>
2023-05-02 14:12:42 -0500

    Ejercicio 1

---
+++ src/ec/edu/intsuperior/vista/Aplication.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.intsuperior.vista;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Aplication {
+    public static void main(String[] args){
+        
+    }
+        public static void meto1(){
+                Scanner leer= new Scanner(system.in);
+                int i=1;
+                while(i<=5){
+                    System.out.println(i);
+                }
+}
+}
